8f1b76c57a63df2e1c874572b284a2fe904392ed
marcelldac <dacmarcell@proton.me>
2023-08-13 01:35:49 -0300

    feat: cria estrutura inicial da api

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+	"time"
+)
 
 func main() {
-	fmt.Println("Olá, mundo!")
+
+	http.HandleFunc("/", getTransactions)
+
+	http.ListenAndServe(":8080", nil)
+
+}
+
+type Transaction struct {
+	Title     string
+	Amount    float32
+	Type      int
+	CreatedAt time.Time
+}
+
+type Transactions []Transaction
+
+func getTransactions(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
 }
