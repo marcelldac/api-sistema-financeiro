41647d251061460b3a229e0ecf179c65fe689380
marcelldac <dacmarcell@proton.me>
2023-08-13 02:21:11 -0300

    refactor: modifica local das funcs

---
+++ dio-expert-session-finance/adapter/http/transaction/main.go
@@ -0,0 +1,48 @@
+package transaction
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+
+	"github.com/marcelldac/praticando-golang/dio-expert-session-finance/model/transaction"
+)
+
+func GetTransactions(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	var layout = "2006-01-02T15:04:05"
+	salaryReceived, _ := time.Parse(layout, "2020-04-05T11:45:26")
+
+	var transactions = transaction.Transactions{
+		transaction.Transaction{
+			Title:     "Salário",
+			Amount:    1200.0,
+			Type:      0,
+			CreatedAt: salaryReceived,
+		},
+	}
+
+	_ = json.NewEncoder(w).Encode(transactions)
+}
+
+func CreateATransaction(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	var res = transaction.Transactions{}
+	var body, _ = io.ReadAll(r.Body)
+
+	_ = json.Unmarshal(body, &res)
+
+	fmt.Print(res)
+}
--- main.go
+++ main.go
@@ -1,57 +1,16 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"io"
 	"net/http"
-	"time"
 
-	"github.com/marcelldac/praticando-golang/dio-expert-session-finance/model/transaction"
+	"github.com/marcelldac/praticando-golang/dio-expert-session-finance/adapter/http/transaction"
 )
 
 func main() {
 
-	http.HandleFunc("/transactions", getTransactions)
-	http.HandleFunc("/transactions/create", createATransaction)
+	http.HandleFunc("/transactions", transaction.GetTransactions)
+	http.HandleFunc("/transactions/create", transaction.CreateATransaction)
 
 	http.ListenAndServe(":8080", nil)
 
-}
-
-func getTransactions(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "GET" {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-
-	var layout = "2006-01-02T15:04:05"
-	salaryReceived, _ := time.Parse(layout, "2020-04-05T11:45:26")
-
-	var transactions = transaction.Transactions{
-		transaction.Transaction{
-			Title:     "Salário",
-			Amount:    1200.0,
-			Type:      0,
-			CreatedAt: salaryReceived,
-		},
-	}
-
-	_ = json.NewEncoder(w).Encode(transactions)
-}
-
-func createATransaction(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		return
-	}
-
-	var res = transaction.Transactions{}
-	var body, _ = io.ReadAll(r.Body)
-
-	_ = json.Unmarshal(body, &res)
-
-	fmt.Print(res)
 }
