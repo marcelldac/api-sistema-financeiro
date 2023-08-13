6ffeaed21454a1aa9b3475368e82a37fe15b217c
marcelldac <dacmarcell@proton.me>
2023-08-13 02:12:16 -0300

    feat: adiciona metodo POST

--- main.go
+++ main.go
@@ -1,23 +1,27 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io"
 	"net/http"
 	"time"
 )
 
 func main() {
 
-	http.HandleFunc("/", getTransactions)
+	http.HandleFunc("/transactions", getTransactions)
+	http.HandleFunc("/transactions/create", createATransaction)
 
 	http.ListenAndServe(":8080", nil)
 
 }
 
 type Transaction struct {
-	Title     string
-	Amount    float32
-	Type      int
-	CreatedAt time.Time
+	Title     string    `json:"title"`
+	Amount    float32   `json:"amount"`
+	Type      int       `json:"type"`
+	CreatedAt time.Time `json:"created_at"`
 }
 
 type Transactions []Transaction
@@ -27,4 +31,34 @@ func getTransactions(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		return
 	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	var layout = "2006-01-02T15:04:05"
+	salaryReceived, _ := time.Parse(layout, "2020-04-05T11:45:26")
+
+	var transactions = Transactions{
+		Transaction{
+			Title:     "Salário",
+			Amount:    1200.0,
+			Type:      0,
+			CreatedAt: salaryReceived,
+		},
+	}
+
+	_ = json.NewEncoder(w).Encode(transactions)
+}
+
+func createATransaction(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	var res = Transactions{}
+	var body, _ = io.ReadAll(r.Body)
+
+	_ = json.Unmarshal(body, &res)
+
+	fmt.Print(res)
 }
