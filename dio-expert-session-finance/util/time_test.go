365a43c8ddbf97678f143c8dafcf064f8d4dfb87
marcelldac <dacmarcell@proton.me>
2023-08-14 09:05:22 -0300

    feat(time_test): adiciona teste de mes e hora

--- dio-expert-session-finance/util/time_test.go
+++ dio-expert-session-finance/util/time_test.go
@@ -8,4 +8,12 @@ func TestStringToTime(t *testing.T) {
 	if convertedTime.Year() != 2019 {
 		t.Errorf("Espera que o ano seja 2019")
 	}
+
+	if convertedTime.Month() != 02 {
+		t.Errorf("Espera que o mês seja 02")
+	}
+
+	if convertedTime.Hour() != 10 {
+		t.Errorf("Espera que o hora seja 10")
+	}
 }
