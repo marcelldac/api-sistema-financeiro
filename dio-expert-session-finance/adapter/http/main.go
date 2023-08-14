bb5ef750b473ef72665a13f3e4fcc0cf7320b97d
marcelldac <dacmarcell@proton.me>
2023-08-13 23:31:19 -0300

    feat: adiciona endpoint health

--- dio-expert-session-finance/adapter/http/main.go
+++ dio-expert-session-finance/adapter/http/main.go
@@ -3,11 +3,13 @@ package http
 import (
 	"net/http"
 
+	"github.com/marcelldac/praticando-golang/dio-expert-session-finance/adapter/http/actuator"
 	"github.com/marcelldac/praticando-golang/dio-expert-session-finance/adapter/http/transaction"
 )
 
 func Init() {
 	http.HandleFunc("/transactions", transaction.GetTransactions)
 	http.HandleFunc("/transactions/create", transaction.CreateATransaction)
+	http.HandleFunc("/health", actuator.Health)
 	http.ListenAndServe(":8080", nil)
 }
