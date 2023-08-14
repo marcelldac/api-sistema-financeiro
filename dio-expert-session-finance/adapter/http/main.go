23ac2550ca6b85babc4f6467f72a91e173f0da44
marcelldac <dacmarcell@proton.me>
2023-08-14 09:24:06 -0300

    feat: adiciona endpoint /metrics
    - Prometheus

--- dio-expert-session-finance/adapter/http/main.go
+++ dio-expert-session-finance/adapter/http/main.go
@@ -5,11 +5,13 @@ import (
 
 	"github.com/marcelldac/praticando-golang/dio-expert-session-finance/adapter/http/actuator"
 	"github.com/marcelldac/praticando-golang/dio-expert-session-finance/adapter/http/transaction"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func Init() {
 	http.HandleFunc("/transactions", transaction.GetTransactions)
 	http.HandleFunc("/transactions/create", transaction.CreateATransaction)
 	http.HandleFunc("/health", actuator.Health)
+	http.Handle("/metrics", promhttp.Handler())
 	http.ListenAndServe(":8080", nil)
 }
