37c7b46bd534b1a66e0c7af19bd936e12c9eab2c
marcelldac <dacmarcell@proton.me>
2023-08-18 18:27:02 -0300

    feat: adiciona endpoint '/'
    - esse endpoit retorna um log indicando o endpoint 'correto'

--- adapter/http/main.go
+++ adapter/http/main.go
@@ -1,6 +1,7 @@
 package http
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/marcelldac/praticando-golang/adapter/http/actuator"
@@ -8,7 +9,16 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
+func mainResponse(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+	fmt.Printf("===========================API de Sistema financeiro=============================\nOlá, seja bem vindo! Você pode ver os endpoints na documentação. Atualmente o principal é o GET /transactions.\n=================================================================================")
+}
+
 func Init() {
+	http.HandleFunc("/", mainResponse)
 	http.HandleFunc("/transactions", transaction.GetTransactions)
 	http.HandleFunc("/transactions/create", transaction.CreateATransaction)
 	http.HandleFunc("/health", actuator.Health)
